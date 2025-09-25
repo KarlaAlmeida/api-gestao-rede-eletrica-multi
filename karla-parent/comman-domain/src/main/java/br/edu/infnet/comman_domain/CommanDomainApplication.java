6e3b6bd0125661423488da521605da93597a90ca
KarlaAlmeida <karlakcbam@gmail.com>
2025-09-25 20:36:25 -0300

    criada estrutura modulos e configurado pom do parent

---
+++ karla-parent/comman-domain/src/main/java/br/edu/infnet/comman_domain/CommanDomainApplication.java
@@ -0,0 +1,13 @@
+package br.edu.infnet.comman_domain;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommanDomainApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CommanDomainApplication.class, args);
+	}
+
+}
