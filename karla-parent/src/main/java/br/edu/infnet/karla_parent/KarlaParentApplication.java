a7958cde1bb214435fc8663907a8df92f86602f2
KarlaAlmeida <karlakcbam@gmail.com>
2025-09-25 17:48:20 -0300

    first commit

---
+++ karla-parent/src/main/java/br/edu/infnet/karla_parent/KarlaParentApplication.java
@@ -0,0 +1,13 @@
+package br.edu.infnet.karla_parent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KarlaParentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KarlaParentApplication.class, args);
+	}
+
+}
---
+++ karla-parent/src/test/java/br/edu/infnet/karla_parent/KarlaParentApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.infnet.karla_parent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KarlaParentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
